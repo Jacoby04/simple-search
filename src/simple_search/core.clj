39152ce14867794947cfa94ede529563ff5ac643
Jacoby04 <opdah023@morris.umn.edu>
2016-02-05 14:31:01 -0600

    Moved things around. Definitely not working entirely yet to remove things.

--- src/simple_search/core.clj
+++ src/simple_search/core.clj
@@ -31,30 +31,6 @@
      :total-weight (reduce + (map :weight included))
      :total-value (reduce + (map :value included))}))
 
-(defn remove-random-choice
-  "Removes a random choice from choices in an instance"
-  [instance]
-  (let [choices (find-and-remove-choice (instance :choices))
-        included (included-items (:items instance) choices)]
-  {:instance instance
-   :choices choices
-   :total-weight (reduce + (map :weight included))
-   :total-value (reduce + (map :value included))}))
-
-(defn find-and-remove-choice
-  "Takes a list of choices and returns the same list with one choice removed randomly."
-  [choices]
-  (def choicesVector (vec choices))
-  (loop [rand #(rand-int (count choicesVector))]
-    (if (= (get choicesVector rand) 1)
-      (lazy-seq (assoc choicesVector rand 0))
-      (recur (#(rand-int (count choicesVector))))
-      )
-  )
-  )
-
-(find-and-remove-choice '(0 1 0 0 0 0 1 0 0 0 0 0 1))
-
 ;;; It might be cool to write a function that
 ;;; generates weighted proportions of 0's and 1's.
 
@@ -62,7 +38,7 @@
 ;;; We modified the score such that answers that are overweight return negative their score.
 (defn score
   "Takes the total-weight of the given answer unless it's over capacity,
-   in which case we return 0."
+   in which case we return the negative of what the value would be."
   [answer]
   (if (> (:total-weight answer)
          (:capacity (:instance answer)))
@@ -106,6 +82,32 @@
 
 (remove-then-random-replace (random-answer knapPI_16_20_1000_1))
 
+(defn remove-random-choice
+  "Removes a random choice from choices in an instance"
+  [instance]
+  (let [choices (find-and-remove-choice (instance :choices))
+        included (included-items (:items instance) choices)]
+  {:instance instance
+   :choices choices
+   :total-weight (reduce + (map :weight included))
+   :total-value (reduce + (map :value included))}))
+
+;;; Pretty sure this isn't returning the right thing....
+(defn find-and-remove-choice
+  "Takes a list of choices and returns the same list with one choice removed randomly."
+  [choices]
+  (def choicesVector (vec choices))
+  (loop [rand #(rand-int (count choicesVector))]
+    (if (= (get choicesVector rand) 1)
+      (lazy-seq (assoc choicesVector rand 0))
+      (recur (#(rand-int (count choicesVector))))
+      )
+  )
+  )
+
+(find-and-remove-choice '(0 1 0 0 0 0 1 0 0 0 0 0 1))
+
+(remove-random-choice (random-answer knapPI_16_20_1000_1))
 
 
 
